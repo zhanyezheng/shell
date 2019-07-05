3512a81af3fd7bd33166964650231cd21bc0555b
zzy <1002081237@qq.com>
2019-07-05 21:25:58 +0800

    bash

---
+++ 03.0_genotype_pcs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/f/Tools/plink2/plink-1.90/plink --bfile /f/mulinlab/zhanye/project/tset/vcf/all.vcf --pca --out /f/mulinlab/zhanye/project/tset/vcf/genoty_pcs
---
+++ vcf_to_bed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#在主目录下运行
+./plink2 --vcf ./project/tset/vcf/all.vcf.gz --make-bed --out ./project/tset/vcf/all.vcf  #all.vcf 生成文件的前缀
